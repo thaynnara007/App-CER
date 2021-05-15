b9fbbed8c1ffb3049438efec7d670e8beda031a1
Thaynnara <thaynnara.goncalves@ccc.ufcg.edu.br>
2021-05-14 21:16:15 -0300

    add font family to the button

--- app/src/main/java/com/example/app_cer/MainActivity.java
+++ app/src/main/java/com/example/app_cer/MainActivity.java
@@ -1,14 +1,24 @@
 package com.example.app_cer;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button letsGo;
+    Typeface quicksand;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        letsGo = findViewById(R.id.letsGoButton);
+        quicksand = ResourcesCompat.getFont(getBaseContext(), R.font.quicksand_medium);
+        letsGo.setTypeface(quicksand);
     }
 }
