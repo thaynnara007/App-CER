6994c2b97636fe765ee9cc71f62b30b73da47891
Thaynnara <thaynnara.goncalves@ccc.ufcg.edu.br>
2021-05-22 18:16:00 -0300

    finish register layout

--- app/src/main/java/com/example/app_cer/activity/RegisterActivity.java
+++ app/src/main/java/com/example/app_cer/activity/RegisterActivity.java
@@ -1,16 +1,48 @@
 package com.example.app_cer.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.app_cer.R;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    EditText nameInput;
+    EditText genderInput;
+    EditText ageInput;
+    EditText phoneInput;
+    EditText emailInput;
+    EditText passwordInput;
+    Button registerButton;
+    Typeface quicksand;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        nameInput = findViewById(R.id.nameInput);
+        genderInput = findViewById(R.id.genderInput);
+        ageInput = findViewById(R.id.ageInput);
+        phoneInput = findViewById(R.id.phoneInput);
+        emailInput = findViewById(R.id.emailRegisterInput);
+        passwordInput = findViewById(R.id.passwordRegisterInput);
+        registerButton = findViewById(R.id.registerPageButton);
+
+        quicksand = ResourcesCompat.getFont(getApplicationContext(), R.font.quicksand_medium);
+
+        nameInput.setTypeface(quicksand);
+        genderInput.setTypeface(quicksand);
+        ageInput.setTypeface(quicksand);
+        phoneInput.setTypeface(quicksand);
+        emailInput.setTypeface(quicksand);
+        passwordInput.setTypeface(quicksand);
+        registerButton.setTypeface(quicksand);
+
     }
 }
