154c703d4875e4a66c8d2e837aab6be0a47fdbbe
Thaynnara <thaynnara.goncalves@ccc.ufcg.edu.br>
2021-05-22 20:24:08 -0300

    fiz sizes and change ids

--- app/src/main/java/com/example/app_cer/activity/EditRegisterActivity.java
+++ app/src/main/java/com/example/app_cer/activity/EditRegisterActivity.java
@@ -1,16 +1,47 @@
 package com.example.app_cer.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.app_cer.R;
 
 public class EditRegisterActivity extends AppCompatActivity {
 
+    EditText nameInput;
+    EditText genderInput;
+    EditText ageInput;
+    EditText phoneInput;
+    EditText emailInput;
+    EditText passwordInput;
+    Button editButton;
+    Typeface quicksand;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_register);
+
+        nameInput = findViewById(R.id.nameInputEdit);
+        genderInput = findViewById(R.id.genderInputEdit);
+        ageInput = findViewById(R.id.ageInputEdit);
+        phoneInput = findViewById(R.id.phoneInputEdit);
+        emailInput = findViewById(R.id.emailRegisterInputEdit);
+        passwordInput = findViewById(R.id.passwordRegisterInputEdit);
+        editButton = findViewById(R.id.registerEditButton);
+
+        quicksand = ResourcesCompat.getFont(getApplicationContext(), R.font.quicksand_medium);
+
+        nameInput.setTypeface(quicksand);
+        genderInput.setTypeface(quicksand);
+        ageInput.setTypeface(quicksand);
+        phoneInput.setTypeface(quicksand);
+        emailInput.setTypeface(quicksand);
+        passwordInput.setTypeface(quicksand);
+        editButton.setTypeface(quicksand);
     }
 }
