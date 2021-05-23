fb6aa9ccc03805ac0c8599b523f33a9eed646849
Thaynnara <thaynnara.goncalves@ccc.ufcg.edu.br>
2021-05-22 21:06:08 -0300

    padronize text style

---
+++ app/src/main/java/com/example/app_cer/activity/MenuActivity.java
@@ -0,0 +1,17 @@
+package com.example.app_cer.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.app_cer.R;
+
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
