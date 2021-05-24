ca657a46ecd5444f35c2b617ccc876577eb51ff6
Thaynnara <thaynnara.goncalves@ccc.ufcg.edu.br>
2021-05-24 20:28:18 -0300

    finish layout

--- app/src/main/java/com/example/app_cer/activity/DailyLifeActivity.java
+++ app/src/main/java/com/example/app_cer/activity/DailyLifeActivity.java
@@ -63,7 +63,9 @@ public class DailyLifeActivity extends AppCompatActivity {
         View.OnClickListener onClickListenerHygine = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(context, "CONTROLE", Toast.LENGTH_SHORT).show();
+                Intent goToHygineActivity = new Intent(context, HygineActivity.class);
+
+                startActivity(goToHygineActivity);
             }
         };
 
--- app/src/main/java/com/example/app_cer/activity/HygineActivity.java
+++ app/src/main/java/com/example/app_cer/activity/HygineActivity.java
@@ -1,16 +1,86 @@
 package com.example.app_cer.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.app_cer.R;
+import com.example.app_cer.adapter.OptionAdapter;
+import com.example.app_cer.model.Option;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HygineActivity extends AppCompatActivity {
+
+    private Button backButton;
+    private RecyclerView recyclerViewOptions;
+    private List<Option> options = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hygine);
+
+        backButton = findViewById(R.id.hygineBackButton);
+        recyclerViewOptions = findViewById(R.id.recyclerViewHygineOptions);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerViewOptions.setLayoutManager(layoutManager);
+
+        this.generateOptions();
+
+        OptionAdapter adapter = new OptionAdapter(options);
+        recyclerViewOptions.setAdapter(adapter);
+    }
+
+    private void generateOptions() {
+        final Context context = getApplicationContext();
+
+        View.OnClickListener onClickListenerBody = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(context, "CONTROLE", Toast.LENGTH_SHORT).show();
+            }
+        };
+
+        View.OnClickListener onClickListenerTeeth = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(context, "CONTROLE", Toast.LENGTH_SHORT).show();
+            }
+        };
+
+
+        Option optBody = new Option(
+                "Corpo",
+                "Atividade responsável por estímulos motores, sensoriais e corporais.",
+                R.drawable.body,
+                onClickListenerBody);
+
+        Option optTeeth = new Option(
+                "Dentes",
+                "Atividade responsável por estímulos motores, sensoriais e corporais.",
+                R.drawable.tooth,
+                onClickListenerTeeth);
+
+
+        this.options.add(optBody);
+        this.options.add(optTeeth);
+
     }
 }
