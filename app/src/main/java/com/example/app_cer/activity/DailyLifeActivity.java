02430519a5259bcd277201fcba73939b3fbf3ec0
Thaynnara <thaynnara.goncalves@ccc.ufcg.edu.br>
2021-05-24 21:01:13 -0300

    finish layout

--- app/src/main/java/com/example/app_cer/activity/DailyLifeActivity.java
+++ app/src/main/java/com/example/app_cer/activity/DailyLifeActivity.java
@@ -72,7 +72,9 @@ public class DailyLifeActivity extends AppCompatActivity {
         View.OnClickListener onClickListenerClothes = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(context, "CONTROLE", Toast.LENGTH_SHORT).show();
+                Intent goToClothesActivity = new Intent(context, ClothesActivity.class);
+
+                startActivity(goToClothesActivity);
             }
         };
 
