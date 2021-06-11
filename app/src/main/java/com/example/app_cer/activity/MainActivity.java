c719941e9f2ad373911330b608e3b1e76a99b254
Thaynnara <thaynnara.goncalves@ccc.ufcg.edu.br>
2021-06-11 16:56:56 -0300

    finish main activity

--- app/src/main/java/com/example/app_cer/activity/MainActivity.java
+++ app/src/main/java/com/example/app_cer/activity/MainActivity.java
@@ -1,41 +1,16 @@
 package com.example.app_cer.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.res.ResourcesCompat;
 
-import android.content.Intent;
-import android.graphics.Typeface;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import com.example.app_cer.R;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button letsGo;
-    private Typeface quicksand;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        letsGo = findViewById(R.id.letsGoButton);
-
-        quicksand = ResourcesCompat.getFont(getBaseContext(), R.font.quicksand_medium);
-        letsGo.setTypeface(quicksand, Typeface.BOLD);
-
-        letsGo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent goToLoginActivity = new Intent(
-                        getApplicationContext(),
-                        Login.class
-                );
-
-                startActivity(goToLoginActivity);
-            }
-        });
     }
 }
