ac619499567e1bda2265421018a98aa7d17f5152
Thaynnara <thaynnara.goncalves@ccc.ufcg.edu.br>
2021-06-14 11:12:09 -0300

    add triangles at begin activity

--- app/src/main/java/com/example/app_cer/activity/MainActivity.java
+++ app/src/main/java/com/example/app_cer/activity/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.app_cer.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import com.example.app_cer.R;
 
@@ -12,5 +14,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Handler handler = new Handler();
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent goToBeginActivity = new Intent(
+                        getApplicationContext(),
+                        BeginActivity.class);
+
+                startActivity(goToBeginActivity);
+            }
+        }, 2000);
+
     }
 }
