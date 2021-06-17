b295269b43b1b98103d4cafb7cea010a29f22ffd
Thaynnara <thaynnara.goncalves@ccc.ufcg.edu.br>
2021-06-17 18:50:34 -0300

    updated some constants

--- app/src/main/java/com/example/app_cer/utils/Constants.java
+++ app/src/main/java/com/example/app_cer/utils/Constants.java
@@ -7,9 +7,9 @@ public class Constants {
     public static final String CLOTHES_NAME = "Roupas";
     public static final String BODY_NAME = "Corpo";
     public static final String TEETH_NAME = "Dentes";
-    public static final String LIQUID_NAME = "Líquido";
-    public static final String SOLID_NAME = "Sólido";
-    public static final String PASTY_NAME = "Pastoso";
+    public static final String LIQUID_NAME = "Líquidos";
+    public static final String SOLID_NAME = "Sólidos";
+    public static final String PASTY_NAME = "Pastosos";
     public static final String SHIRT_NAME = "Blusa";
     public static final String PANTS_NAME = "Calça";
 }
